f83cc68b4f509ed48be80762ba108edf1b52b8b4
Danry Ague <74456102+epcraft93@users.noreply.github.com>
2021-01-27 15:04:09 +0800

    added svg-2 animations

--- assets/js/obs.js
+++ assets/js/obs.js
@@ -6,18 +6,41 @@ const testOptn = {
   // rootMargin: "0px 0px -250px 0px"
 };
 
-const testObs = new IntersectionObserver(function(el, testObs) {
+const obs_svg1 = new IntersectionObserver(function(el, testObs) {
   el.forEach((e, i) => {
     console.log("Page loaded.")
     if (!e.isIntersecting) {
       return;
     } else {
-      setTimeout(() => {e.target.classList.add("anim-once");}, 3000);
-      setTimeout(() => {e.target.classList.remove("anim-once");}, 5000);
+      console.log(e.target);
+      setTimeout(() => {e.target.classList.add("anim-once");}, 2000);
+      setTimeout(() => {e.target.classList.remove("anim-once");}, 4000);
       testObs.unobserve(e.target);
     }
   });
 },
 testOptn);
 
-document.onload = testObs.observe(toAnimate);
+document.onload = obs_svg1.observe(toAnimate);
+
+const toAnimate2 = document.querySelector(".svg-2");
+
+testOptn2 = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const obs_svg2 = new IntersectionObserver(function(el, testObs) {
+  el.forEach((e, i) => {
+    if (!e.isIntersecting) {
+      return;
+    } else {
+      console.log(e.target);
+      setTimeout(() => {e.target.classList.add("animate");console.log('added');}, 250);
+      testObs.unobserve(e.target);
+    }
+  });
+},
+testOptn2);
+
+document.onload = obs_svg2.observe(toAnimate2);
